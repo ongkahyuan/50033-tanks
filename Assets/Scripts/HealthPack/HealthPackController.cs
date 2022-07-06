afda80f9c8e6501cb366d3d140df109b511d6c71
ongkahyuan <ongkahyuan@gmail.com>
2022-07-06 17:37:17 +0800

    Add health pack
    Also make everyone the enemy

---
+++ Assets/Scripts/HealthPack/HealthPackController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthPackController : MonoBehaviour
+{
+    private BoxCollider colliderComp;
+    public UnityEvent onHealthPickup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliderComp = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            onHealthPickup.Invoke();
+            Debug.Log("Got items");
+            Destroy(gameObject);
+        }
+    }
+}
